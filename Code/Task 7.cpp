82f86d463e5f3b63d32ba8d9a76da93cddc0a2fe
zaiyan-fast <zaiyanumer6@gmail.com>
2024-09-14 17:27:46 +0500

    Add files via upload

---
+++ Code/Task 7.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	printf("\t\t\t\t\t\t     __________________    \n");
+	printf("\t\t\t\t\t\t    \/\t\t       \\\n");
+	printf("\t\t\t\t\t\t   \/   \/\\\_\/\\\t\t\\\n");
+	printf("\t\t\t\t\t\t  \/   ( \. \. )  \^ \t \\\n");
+	printf("\t\t\t\t\t\t \/     )   (  ((\t  \\\n");
+	printf("\"cat\"\t\t\t\t\t\t\|     ( \> \< )\_))\t   \|\n ");
+	printf("\t\t\t\t\t\t \\     -----\t\t  \/\n");
+	printf("\t\t\t\t\t\t  \\      24K-0771\t \/\n");
+	printf("\t\t\t\t\t\t   \\\t\t\t\/\n");
+	printf("\t\t\t\t\t\t    \\__________________\/");
+	return 0;
+}
